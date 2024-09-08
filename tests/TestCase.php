f63fe68f59096b2b16e58f04d8b0f4e85f9e8ec7
CrazyBoy49z <yura.finiv@gmail.com>
2024-09-08 13:30:26 +0300

    Add RefreshDatabase and WithFaker traits to TestCase
    Included RefreshDatabase and WithFaker traits to improve testing capabilities in TestCase. Also added a config setting for the lazy setting table to ensure consistent table naming in tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,16 @@
 namespace Step2Dev\LazySetting\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Step2Dev\LazySetting\LazySettingServiceProvider;
 
 class TestCase extends Orchestra
 {
+    use RefreshDatabase;
+    use WithFaker;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -28,8 +33,10 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
         config()->set('database.connections.testing.prefix', '');
+        config()->set('lazy.setting.table', 'settings');
 
         $migration = include __DIR__.'/../database/migrations/create_lazy_setting_table.php.stub';
         $migration->up();
+
     }
 }
