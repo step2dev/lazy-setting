e54d66fcd0c20db3854c357baf3799e3deb7d526
CrazyBoy49z <CrazyBoy49z@users.noreply.github.com>
2024-09-08 09:38:20 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schema;
 use Step2Dev\LazySetting\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
